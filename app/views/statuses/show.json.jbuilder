8c3e00096a29303ad5b1da1f38e6f2859b534e17
Argie Gallego <argie_gallego@yahoo.com>
2014-01-22 04:41:17 -0800

    new app using postgresql

---
+++ app/views/statuses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @status, :id, :Name, :Message, :created_at, :updated_at
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WwwAratalksCom::Application.config.session_store :cookie_store, key: '_www_aratalks_com_session'
